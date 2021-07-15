0db1bc08042d2138404c2e1a98597df0297323c8
unknown <shivam170620@gmail.com>
2021-07-15 19:11:42 +0530

    init

---
+++ calc/calculator.js
@@ -0,0 +1,62 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenvalue='';
+for(item of buttons)
+{
+
+	item.addEventListener('click',(e)=>
+	{
+        buttontext=e.target.innerText;
+        console.log('Button text pressed is ', buttontext);
+        if(buttontext=='X')
+        {
+        	buttontext= '*';
+             screenvalue+=buttontext;
+             screen.value=screenvalue;
+        }
+        else
+        	if(buttontext=='C')
+        	{
+        		screenvalue='';
+        		screen.value=screenvalue;
+        	}
+        	else
+        		if(buttontext=='=')
+        		{
+        			screen.value=eval(screenvalue);
+        		}
+        		else
+        			if(buttontext=='sin')
+        			{
+        				screenvalue=Math.sin(screen.value);
+        		   screen.value=screenvalue;
+                       }
+        			
+        			else
+        			if(buttontext=='cos')
+        			{
+        				screenvalue=Math.cos(screen.value);
+        		   screen.value=screenvalue;
+
+        			}
+        			else
+        			if(buttontext=='tan')
+        			{
+        				screenvalue=Math.tan(screen.value);
+        		   screen.value=screenvalue;
+
+        	}
+                 else
+                 	if(buttontext=='log')
+                 	{
+                 		screenvalue=Math.log(screen.value);
+        		   screen.value=screenvalue;
+
+                 	}
+        	else
+        		{
+        			  screenvalue+=buttontext;
+             screen.value=screenvalue;
+        		}
+	});
+}
